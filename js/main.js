df799a1edf3eda011ea40ee6da4a0984d9da2cd8
AmandaKhol <amanda.camsua@gmail.com>
2021-02-09 13:18:50 +0100

    add new function to render feedback

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict';
 
-const buttonElement = document.querySelector(".js-button");
-const inputElement = document.querySelector(".js-input");
-const clueElement = document.querySelector(".js-clue");
-const tryElement = document.querySelector(".js-try");
+const buttonElement = document.querySelector('.js-button');
+const inputElement = document.querySelector('.js-input');
+const clueElement = document.querySelector('.js-clue');
+const tryElement = document.querySelector('.js-try');
 
 let tryCount = 0;
 const maxNumber = 100;
@@ -25,17 +25,34 @@ function increaseAndPrintCount() {
 // Random number generation
 const solutionNumber = getRandomNumber(minNumber, maxNumber);
 
+function renderFeedback(responseInt) {
+  if (responseInt === solutionNumber) {
+    clueElement.innerHTML = `¡¡Has ganado campeona!`;
+    increaseAndPrintCount();
+  } else if (responseInt < solutionNumber) {
+    clueElement.innerHTML = `Pista: Demasiado bajo`;
+    increaseAndPrintCount();
+  } else {
+    messageToClue(`Pista: Demasiado alto`);
+    increaseAndPrintCount();
+  }
+}
+
+function messageToClue(message) {
+  clueElement.innerHTML = message;
+}
+
 //Button handle
 function handleBtn() {
   const response = inputElement.value;
   const responseInt = parseInt(response);
-  const responseIntToString = responseInt + "";
+  const responseIntToString = responseInt + '';
 
   //Check errors in input data: avoid letters or void value
   if (
     isNaN(responseInt) ||
     responseIntToString !== response ||
-    response === ""
+    response === ''
   ) {
     clueElement.innerHTML = `Pista: Escribe un número entre ${minNumber} y ${maxNumber}`;
     return;
@@ -45,17 +62,7 @@ function handleBtn() {
     clueElement.innerHTML = `Pista: El número debe estar entre  ${minNumber} y ${maxNumber}`;
     return;
   }
-
-  if (responseInt === solutionNumber) {
-    clueElement.innerHTML = `¡¡Has ganado campeona!`;
-    increaseAndPrintCount();
-  } else if (responseInt < solutionNumber) {
-    clueElement.innerHTML = `Pista: Demasiado bajo`;
-    increaseAndPrintCount();
-  } else {
-    clueElement.innerHTML = `Pista: Demasiado alto`;
-    increaseAndPrintCount();
-  }
+  renderFeedback(responseInt);
 }
 
-buttonElement.addEventListener("click", handleBtn);
+buttonElement.addEventListener('click', handleBtn);
